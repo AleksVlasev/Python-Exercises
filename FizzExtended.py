b4b2993a7e309a0450129a55a5e70aea90550938
Aleksandar Vlasev <avlasev@gmail.com>
2015-05-23 16:33:35 -0700

    Added comments in FizzExtended

--- FizzExtended.py
+++ FizzExtended.py
@@ -1,14 +1,19 @@
 __author__ = 'Vlasev'
-
-'''In this exercise we'll implement a Fizz-Buzz-like procedure for any input array of number conditions'''
+'''Let's be honest here. The fizz-buzz exercise is nothing more than an exercise in listing
+	all the prime divisors of a number, where instead of the factors we're printing fizz/buzz or a combination.
+	Just for fun we'll implement a general procedure for highlighting if numbers in a list have specific prime factors'''
 
 def fizz(num, k):
+	'''This function just tests if num is divisible by k'''
 	if num % k == 0:
 		return True
 	else:
 		return False
 
+
 def num_fizz(num, array):
+	'''For a given number num and a collection of numbers in array, we iteratively build the label that we'll
+	have for num.'''
 	marked = False
 	label = ""
 	for a in array:
@@ -20,11 +25,14 @@ def num_fizz(num, array):
 	if marked:
 		return "{} is divisible by ".format(num) + label
 	else:
-		return "{} is not divisible by any numbers in array.".format(num)
+		return "{} is not divisible by any number in array.".format(num)
+
 
 def full_fizz(low, high, array):
 	print("\nThe numbers to test are {} - {} and the array is ".format(low, high) + str(array) + "\n")
 	for num in range(low, high):
 		print(num_fizz(num, array))
 
-full_fizz(1, 101, [2, 3, 5, 7])
+full_fizz(1, 101, [3, 5])  # Original Fizz-Buzz-like exercise
+full_fizz(1, 201, [2, 3, 5, 7])  # Extending to more inputs
+full_fizz(1, 201, [2, 6, 5, 10])  # They don't have to be prime numbers
